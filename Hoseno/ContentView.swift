ccf2fc3157d876cef1d523256af8228d9393e568
Chien Pham <minhchien.pham@foody.vn>
2021-05-08 18:56:47 +0700

    Initial Commit

---
+++ Hoseno/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hoseno
+//
+//  Created by Chien Pham on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
